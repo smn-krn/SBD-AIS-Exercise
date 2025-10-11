5dc961b44ec03e35268ac4e97d453f9a062dfbff
smn-krn <kern.simone@icloud.com>
2025-10-11 23:44:11 +0200

    Assignment02 finished version

--- Exc_2/skeleton/model/drink.go
+++ Exc_2/skeleton/model/drink.go
@@ -3,5 +3,8 @@ package model
 type Drink struct {
 	ID uint64 `json:"id"`
 	// todo Add fields: Name, Price, Description with suitable types
+	Name        string  `json:"name"`
+	Price       float32 `json:"price"`
+	Description string  `json:"description"`
 	// todo json attributes need to be snakecase, i.e. name, created_at, my_variable, ..
 }
